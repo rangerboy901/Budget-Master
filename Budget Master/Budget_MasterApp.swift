d161941c6041ee8f4eb2571dbb8630cce26fc978
rangerboy574 <jdeweese574@gmail.com>
2023-02-17 11:50:50 -0600

    Initial Commit

---
+++ Budget Master/Budget_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_MasterApp.swift
+//  Budget Master
+//
+//  Created by Joseph  DeWeese on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Budget_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
